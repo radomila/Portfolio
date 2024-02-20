366c3a218b5a4bdbe4fa584963117591c40eeef8
Radomila Hejhalová <hejhalova.radomila@gmail.com>
2024-02-20 17:21:10 +0100

    chore: data - timeline

--- src/components/Timeline/data.ts
+++ src/components/Timeline/data.ts
@@ -2,6 +2,46 @@ import { Semester } from '../../types/types';
 
 export const data: Semester[] = [
   {
+    year: '2023-2024 (1st semestr of Masters degree)',
+    title: 'Subjects',
+    subjects: [
+      'Statistical Methods in Data Analysis',
+      'Data Analysis and their visualization',
+      'Skills in Technical Writing',
+      'Basics of programming in Python',
+      'Internet of Thing',
+    ],
+    titleExperience: 'Experience',
+    experience:
+      'Focused on study, learning programming in Python and R and creating my own projects in terms of frontend development, exploring new technologies. ',
+  },
+  {
+    year: '2022-2023 (6th semestr)',
+    title: 'Subjects',
+    subjects: [
+      'Business English 6',
+      'UX',
+      'Product management',
+      'The Bachelor Thesis Seminar 2',
+    ],
+    titleExperience: 'Experience',
+    experience:
+      'Frontend developer on the project Pomáhej Ukrajině using ReactJS, TypeScript and Tailwind.',
+  },
+  {
+    year: '2022-2023 (5th semestr)',
+    title: 'Subjects',
+    subjects: [
+      'Business English 5',
+      'The Bachelor Thesis Seminar 1',
+      'Cyber Security',
+      'Advanced Algorithm Development',
+    ],
+    titleExperience: 'Experience',
+    experience:
+      'Bachelor thesis focused on web performance comparison on the selected component named uuForester between SolidJS and UAF.',
+  },
+  {
     year: '2020-2021 (1st semestr)',
     title: 'Subjects',
     subjects: [
@@ -13,33 +53,6 @@ export const data: Semester[] = [
     titleExperience: 'Experience',
     experience:
       'Basics of programming in JavaScript, data structures and algorithms.',
-  },
-  {
-    year: '2020-2021 (2nd semestr)',
-    title: 'Subjects',
-    subjects: [
-      'Business Development Essentials',
-      'Business English 2',
-      'Mathematic Analysis',
-      'Cloud Application Architecture',
-    ],
-    titleExperience: 'Experience',
-    experience:
-      'Basics of frontend and backend development by using ReactJS and ExpressJS.',
-  },
-  {
-    year: '2021-2022 (3rd semestr)',
-    title: 'Subjects',
-    subjects: [
-      'Frontend Systems',
-      'Backend Systems',
-      'Project Management',
-      'Business English 3',
-      'Discrete Mathematics',
-    ],
-    titleExperience: 'Experience',
-    experience:
-      'Junior Software developer at Cyrkl focused on frontend development using JavaScript, ReactJS and styled-components. Worked on Administration app and Career portal.',
   },
   {
     year: '2021-2022 (4th semestr)',
@@ -56,43 +69,30 @@ export const data: Semester[] = [
       'Junior Software developer at Cyrkl focused on frontend development using JavaScript, ReactJS and styled-components. Worked on Career portal and Homepage.',
   },
   {
-    year: '2022-2023 (5th semestr)',
+    year: '2021-2022 (3rd semestr)',
     title: 'Subjects',
     subjects: [
-      'Business English 5',
-      'The Bachelor Thesis Seminar 1',
-      'Cyber Security',
-      'Advanced Algorithm Development',
+      'Frontend Systems',
+      'Backend Systems',
+      'Project Management',
+      'Business English 3',
+      'Discrete Mathematics',
     ],
     titleExperience: 'Experience',
     experience:
-      'Bachelor thesis focused on web performance comparison on the selected component named uuForester between SolidJS and UAF.',
+      'Junior Software developer at Cyrkl focused on frontend development using JavaScript, ReactJS and styled-components. Worked on Administration app and Career portal.',
   },
   {
-    year: '2022-2023 (6th semestr)',
+    year: '2020-2021 (2nd semestr)',
     title: 'Subjects',
     subjects: [
-      'Business English 6',
-      'UX',
-      'Product management',
-      'The Bachelor Thesis Seminar 2',
+      'Business Development Essentials',
+      'Business English 2',
+      'Mathematic Analysis',
+      'Cloud Application Architecture',
     ],
     titleExperience: 'Experience',
     experience:
-      'Frontend developer on the project Pomáhej Ukrajině using ReactJS, TypeScript and Tailwind.',
-  },
-  {
-    year: '2023-2024 (1th semestr of Masters degree)',
-    title: 'Subjects',
-    subjects: [
-      'Statistical Methods in Data Analysis',
-      'Data Analysis and their visualization',
-      'Skills in Technical Writing',
-      'Basics of programming in Python',
-      'Internet of Thing',
-    ],
-    titleExperience: 'Experience',
-    experience:
-      'Focused on study, learning programming in Python and R and creating my own projects in terms of frontend development, exploring new technologies. ',
+      'Basics of frontend and backend development by using ReactJS and ExpressJS.',
   },
 ];
